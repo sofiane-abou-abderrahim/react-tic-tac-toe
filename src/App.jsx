88c5c751b107375591e8000979cb651046de2a5d
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-03-07 22:57:42 +0100

    20. Tic-Tac-Toe Game: The "Game Over" Screen & Checking for a Draw

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
 import Log from './components/Log.jsx';
+import GameOver from './components/GameOver.jsx';
 import { WINNING_COMBINATIONS } from './winning-combinations.js';
 
 const intialGameBoard = [
@@ -56,6 +57,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer(curActivePlayer => (curActivePlayer === 'X' ? 'O' : 'X'));
     setGameTurns(prevTurns => {
@@ -85,7 +88,7 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
