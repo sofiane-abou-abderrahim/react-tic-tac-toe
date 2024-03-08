e978651700838ce92b1407649308d5e51da79e19
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-03-08 12:22:08 +0100

    22. When NOT To Lift State Up

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({ X: 'Player 1', O: 'Player 2' });
   const [gameTurns, setGameTurns] = useState([]);
   // const [hasWinner, setHasWinner] = useState(false);
   // const [activePlayer, setActivePlayer] = useState('X');
@@ -75,6 +76,15 @@ function App() {
 
   function handleRestart() {
     setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    });
   }
 
   return (
